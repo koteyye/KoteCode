53c651487cfd71e3b6914ae3bac92ed9ec14ca04
Koteyye <koteyye@yandex.ru>
2026-08-08 08:56:23 +0300

    fix(server): provide project handler services

--- packages/server/src/handlers/project.ts
+++ packages/server/src/handlers/project.ts
@@ -7,45 +7,43 @@ import { HttpApiBuilder } from "effect/unstable/httpapi"
 import { Api } from "../api"
 
 export const ProjectHandler = HttpApiBuilder.group(Api, "server.project", (handlers) =>
-  handlers
-    .handle("project.list", () => Project.Service.use((project) => project.list()))
-    .handle("project.update", (ctx) =>
-      Effect.gen(function* () {
-        const project = yield* Project.Service
-        const events = yield* EventV2.Service
-        const info = yield* project.update(ctx.params.projectID, ctx.payload).pipe(
-          Effect.catchTag(
-            "Project.NotFoundError",
-            () =>
-              new ProjectNotFoundError({
-                projectID: ctx.params.projectID,
-                message: `Project not found: ${ctx.params.projectID}`,
-              }),
-          ),
-        )
-        yield* events.publish(Project.Event.Updated, info)
-        return info
-      }),
-    )
-    .handle("project.current", () =>
-      Effect.gen(function* () {
-        const location = yield* Location.Service
-        const project = yield* Project.Service
-        const events = yield* EventV2.Service
-        const opened = yield* project.open(location.directory)
-        const info = (yield* project.list()).find((item) => item.id === opened.id)
-        if (info) yield* events.publish(Project.Event.Updated, info)
-        return { id: opened.id, directory: opened.directory }
-      }),
-    )
-    .handle("project.directories", (ctx) =>
-      Project.Service.use((project) => project.directories({ projectID: ctx.params.projectID })),
-    )
-    .handle("project.repositories", () =>
-      Effect.gen(function* () {
-        const location = yield* Location.Service
-        const project = yield* Project.Service
-        return yield* project.repositories(location.directory)
-      }),
-    ),
+  Effect.gen(function* () {
+    const project = yield* Project.Service
+    const events = yield* EventV2.Service
+
+    return handlers
+      .handle("project.list", () => project.list())
+      .handle("project.update", (ctx) =>
+        Effect.gen(function* () {
+          const info = yield* project.update(ctx.params.projectID, ctx.payload).pipe(
+            Effect.catchTag(
+              "Project.NotFoundError",
+              () =>
+                new ProjectNotFoundError({
+                  projectID: ctx.params.projectID,
+                  message: `Project not found: ${ctx.params.projectID}`,
+                }),
+            ),
+          )
+          yield* events.publish(Project.Event.Updated, info)
+          return info
+        }),
+      )
+      .handle("project.current", () =>
+        Effect.gen(function* () {
+          const location = yield* Location.Service
+          const opened = yield* project.open(location.directory)
+          const info = (yield* project.list()).find((item) => item.id === opened.id)
+          if (info) yield* events.publish(Project.Event.Updated, info)
+          return { id: opened.id, directory: opened.directory }
+        }),
+      )
+      .handle("project.directories", (ctx) => project.directories({ projectID: ctx.params.projectID }))
+      .handle("project.repositories", () =>
+        Effect.gen(function* () {
+          const location = yield* Location.Service
+          return yield* project.repositories(location.directory)
+        }),
+      )
+  }),
 )
